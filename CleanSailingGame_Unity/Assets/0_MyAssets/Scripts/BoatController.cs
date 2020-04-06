2838c65d7e6944e9dab12fc993a94242a9751848
Daniel Garcia Fernandez <daniel.garcia@fundacionctic.org>
2020-04-06 22:55:51 +0200

    [LevelDesign] Added water props

---
+++ CleanSailingGame_Unity/Assets/0_MyAssets/Scripts/BoatController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatController : MonoBehaviour
+{
+
+    public float boatSpeed = 50;
+    public float rotationSpeed = 25;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalAxis = Input.GetAxis("Vertical");
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        Debug.Log(verticalAxis);
+        rb.velocity = (transform.forward * verticalAxis) * boatSpeed; // * Time.deltaTime;//new Vector3(rb.velocity.x, rb.velocity.y, verticalAxis * boatSpeed);
+        transform.Rotate((transform.up * horizontalAxis) * rotationSpeed * Time.fixedDeltaTime);
+    }
+}
